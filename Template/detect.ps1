50b528f0019224f6731efe6eb705c3c34b6cdf82
Joshua Burkholder <ehlodex@users.noreply.github.com>
2023-12-05 15:56:40 -0500

    Update detect.ps1

--- Template/detect.ps1
+++ Template/detect.ps1
@@ -1,22 +1,19 @@
 #==================================================
-# Script Name : detect.ps1
-# Synopsis    : Win32 detection script for ...
-# Description : ...
-# Notes       : ...
-# Last update : yyyy.mm.dd by Your Name Here ...
+# Software : Template
+# Action   : Detect
+# Method   : Chocolatey, winget, registry, etc
+# Updated  : yyyy.mm.dd by Your Name Here ...
 #==================================================
 
 # Chocolatey
 $PackageName = "package"
 [int]$PackagesInstalled = "$((choco list $PackageName | findstr "packages installed.")[0])"
-
-If ($PackagesInstalled -ge 1) {
-  Write-Output "Package $PackageName is installed with count $PackagesInstalled"
-  Exit 0
-} else {
+If ($PackagesInstalled -eq 1) {
   Write-Output "Cound not find $PackageName because count was $PackagesInstalled"
   Exit 1
 }
+Write-Output "Package $PackageName is installed with count $PackagesInstalled"
+Exit 0
 
 
 # MsiExec
@@ -28,14 +25,11 @@ $AppID = "Package.Name"
 try {
   $winget = "$(Resolve-Path "C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller_*_x64__8wekyb3d8bbwe" | Sort-Object -Property Path | Select-Object -Last 1)\winget.exe"
   $installed = (& $winget list --id "$AppID" | findstr "$AppID")
+  If (! $installed) { throw "$AppID is not installed!" }
 } catch {
   $ErrorMsg = $_.Exception.Message
   Write-Error $ErrorMsg
   Exit 1
 }
-If ($installed) {
-  Write-Output "$AppID is installed!"
-  Exit 0
-}
-Write-Output "$AppID is NOT installed!"
-Exit 1
+Write-Output "$AppID is installed!"
+Exit 0
