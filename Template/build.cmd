df9c840e5a7a3a9692e39c87a057d3c83b4f66a9
Joshua Burkholder <ehlodex@users.noreply.github.com>
2023-12-05 15:29:58 -0500

    Update build.cmd

--- Template/build.cmd
+++ Template/build.cmd
@@ -5,4 +5,5 @@ set WinAppUtil=C:\Path\To\IntuneWinAppUtil.exe
 %WinAppUtil% -c %~dp0\source -s install.ps1 -o %~dp0
 
 for %%a in ("%~dp0\.") do set "Package=%%~nxa"
+if exist %~dp0\%Package%.intunewin del %~dp0\%Package%.intunewin
 ren %~dp0\install.intunewin %Package%.intunewin
