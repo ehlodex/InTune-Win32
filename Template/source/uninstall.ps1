9d00d04ad2a8b77af8ae86c66370dc0958fd979f
Joshua Burkholder <ehlodex@users.noreply.github.com>
2023-12-05 14:01:57 -0500

    Create uninstall.ps1

---
+++ Template/source/uninstall.ps1
@@ -0,0 +1,43 @@
+#==================================================
+# Script Name : uninstall.ps1
+# Synopsis    : Remove ...
+# Description : ...
+# Notes       : ...
+# Last update : yyyy.mm.dd by Your Name Here ...
+#==================================================
+
+# Chocolatey
+$PackageName = "package"
+try {
+  Start-Process -FilePath "C:\ProgramData\chocolatey\choco.exe" -ArgumentList "uninstall $PackageName -a -x -y"
+} catch {
+  $ErrorMsg = $_.Exception.Message
+  Write-Error $ErrorMsg
+  Exit 1
+}
+Exit 0
+
+
+# MsiExec
+$MsiName = "installer.msi"
+try {
+  Start-Process "msiexec" -ArgumentList "/x $MsiName /qn" -Wait -NoNewWindow
+} catch {
+  $ErrorMsg = $_.Exception.Message
+  Write-Error $ErrorMsg
+  Exit 1
+}
+Exit 0
+
+
+# WinGet
+$AppID = "Package.Name"
+try {
+  $winget = "$(Resolve-Path "C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller_*_x64__8wekyb3d8bbwe" | Sort-Object -Property Path | Select-Object -Last 1)\winget.exe"
+  & $winget uninstall --id "$AppID" --scope machine --source winget --silent --accept-source-agreements --disable-interactivity --purge --force
+} catch {
+  $ErrorMsg = $_.Exception.Message
+  Write-Error $ErrorMsg
+  Exit 1
+}
+Exit 0
