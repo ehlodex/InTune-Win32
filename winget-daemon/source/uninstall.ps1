782cca53b502cde7d33bcf5d93bdb730de744090
Joshua Burkholder <ehlodex@users.noreply.github.com>
2023-12-08 11:03:19 -0500

    Update uninstall.ps1

--- winget-daemon/source/uninstall.ps1
+++ winget-daemon/source/uninstall.ps1
@@ -7,12 +7,19 @@
 $TaskName = "WinGet Upgrade Daemon"
 $TaskPath = "CompanyName"
 $DaemonPath = "C:\ProgramData\$TaskPath\winget-daemon.ps1"
-
+$DaemonDirectory = Split-Path -parent "$DaemonPath"
 
 If (Test-Path -Path "$DaemonPath") {
-  Remove-Item -Path "$DaemonPath" -Force
+  Remove-Item -Path "$DaemonPath" -Force -Confirm:$False
+}
+
+If (! (Get-ChildItem -Path "$DaemonDirectory")) {
+  Remove-Item $DaemonDirectory -Force -Confirm:$False
 }
 
 try {
   Unregister-ScheduledTask -TaskName "$TaskName" -TaskPath "\$TaskPath\" -Confirm:$False -ErrorAction SilentlyContinue
 } catch { }
+
+Write-Output "Just smile and wave, boys. Smile and wave."
+Exit 0
