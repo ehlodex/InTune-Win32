0efb034b4b7693b8d64d4c2b5716390e557a5c10
Joshua Burkholder <ehlodex@users.noreply.github.com>
2023-12-08 10:33:55 -0500

    Create install.ps1

---
+++ winget-daemon/source/install.ps1
@@ -0,0 +1,34 @@
+#==================================================
+# Software : WinGet Upgrade Daemon
+# Action   : Install
+# Method   : File + Task
+#==================================================
+
+$TaskName = "WinGet Upgrade Daemon"
+$TaskPath = "CompanyName"
+$DaemonPath = "C:\ProgramData\$TaskPath\winget-daemon.ps1"
+$DaemonDirectory = Split-Path -parent $DaemonPath
+
+
+try {
+  If (! (Test-Path -Path "$DaemonDirectory")) {
+    New-Item -Path "$DaemonDirectory" -ItemType Directory -Force
+  }
+  Copy-Item -Path (Split-Path -Leaf $DaemonPath) -Destination "$DaemonPath"
+} catch {
+  $ErrorMsg = $_.Exception.Message
+  Write-Error $ErrorMsg
+  Exit 1
+}
+
+$SchTaskAction   = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-ExecutionPolicy Bypass -File $DaemonPath" -WorkingDirectory "$DaemonDirectory"
+$SchTaskTrigger  = New-ScheduledTaskTrigger -Daily -At 2am
+$SchTaskSettings = New-ScheduledTaskSettingsSet -ExecutionTimeLimit (New-TimeSpan -Hours 2) -DontStopIfGoingOnBatteries -StartWhenAvailable
+
+Register-ScheduledTask -User SYSTEM -RunLevel Highest `
+-Action $SchTaskAction `
+-Trigger $SchTaskTrigger `
+-Settings $SchTaskSettings `
+-TaskPath "$TaskPath" `
+-TaskName "$TaskName" `
+-Description "Unattended upgrade for winget apps"
